1a16c127897c946b02017d897130c763a6a99006
José Campelo <josecampelojr@gmail.com>
2023-07-01 15:18:19 -0300

    Criação da lógica do projeto

--- app/src/main/java/com/campelo/alcoolougasolina/MainActivity.kt
+++ app/src/main/java/com/campelo/alcoolougasolina/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.campelo.alcoolougasolina
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var tietGasolina: TextInputEditText
+    private lateinit var tietAlcool: TextInputEditText
+    private lateinit var btnCalcular: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tietGasolina = findViewById(R.id.tietGasolina)
+        tietAlcool = findViewById(R.id.tietAlcool)
+        btnCalcular = findViewById(R.id.btnCalcular)
+
+        btnCalcular.setOnClickListener {
+            val intent = Intent(this, ResultadoActivity::class.java)
+            val gasolina = tietGasolina.text.toString()
+            val alcool = tietAlcool.text.toString()
+
+            if(gasolina.isNotEmpty() && alcool.isNotEmpty()){
+                intent.putExtra("gasolina", gasolina.toDouble())
+                intent.putExtra("alcool", alcool.toDouble())
+
+                startActivity(intent)
+            }else{
+                Toast.makeText(this, "Digite os campos de texto corretamente", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
--- app/src/main/java/com/campelo/alcoolougasolina/ResultadoActivity.kt
+++ app/src/main/java/com/campelo/alcoolougasolina/ResultadoActivity.kt
@@ -2,10 +2,44 @@ package com.campelo.alcoolougasolina
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class ResultadoActivity : AppCompatActivity() {
+
+    private lateinit var txtGasolina: TextView
+    private lateinit var txtAlcool: TextView
+    private lateinit var txtResultado: TextView
+    private lateinit var btnVoltar: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resultado)
+
+        txtGasolina = findViewById(R.id.txtGasolina)
+        txtAlcool = findViewById(R.id.txtAlcool)
+        txtResultado = findViewById(R.id.txtResultado)
+        btnVoltar = findViewById(R.id.btnVoltar)
+
+        val bundle = intent.extras
+
+        if(bundle != null){
+            val gasolina = bundle.getDouble("gasolina")
+            val alcool = bundle.getDouble("alcool")
+            val resultado = alcool / gasolina
+
+            txtGasolina.text = "Preço da Gasolina: ${gasolina}"
+            txtAlcool.text = "Preço do Álcool: ${alcool}"
+
+            txtResultado.text = if(resultado > 0.7){
+                "Gasolina"
+            }else{
+                "Álcool"
+            }
+        }
+
+        btnVoltar.setOnClickListener {
+            finish()
+        }
     }
 }
